6165ce30b33928ee78b2828f22e43d7991051340
PiyushKasera <piyushkumar0511@gmail.com>
2024-07-09 11:41:47 +0530

    all files added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import {Outlet} from 'react-router-dom'
+import UserContextProvider from './context/UserContextProvider'
+
+export default function App() {
+
+  // let x  = 'rishab@gmail.com'
+  // console.log(x.split('@')[0])
+
+  return (
+   <UserContextProvider>
+     <Navbar/>
+     <Outlet/>
+   </UserContextProvider>
+    
+  )
+}
